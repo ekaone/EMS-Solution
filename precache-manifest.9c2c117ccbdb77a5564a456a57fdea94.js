bdac714b9a48652d84b66cd48fd36b96c87a4a2c
Eka Prasetia <ekaone3033@gmail.com>
2020-06-04 19:34:44 +0700

    Updates

---
+++ precache-manifest.9c2c117ccbdb77a5564a456a57fdea94.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620da7b0982722e3d4f7fb90556a322b",
+    "url": "/EMS-Solution/index.html"
+  },
+  {
+    "revision": "f7929cd8febba6daf360",
+    "url": "/EMS-Solution/static/js/2.d09addb9.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/EMS-Solution/static/js/2.d09addb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6feb3ee5871247f26ef",
+    "url": "/EMS-Solution/static/js/main.763a48f5.chunk.js"
+  },
+  {
+    "revision": "174cb5c3b08ed302beb0",
+    "url": "/EMS-Solution/static/js/runtime-main.02902565.js"
+  },
+  {
+    "revision": "ed7ad4035dbbbc33048c513f4d74f88a",
+    "url": "/EMS-Solution/static/media/Nissan.ed7ad403.png"
+  },
+  {
+    "revision": "6eebd28c455a73c2c4356cee0b9067d0",
+    "url": "/EMS-Solution/static/media/Toyota.6eebd28c.png"
+  },
+  {
+    "revision": "b71663271df71e4ddd4e6e4ba474f1db",
+    "url": "/EMS-Solution/static/media/aboutus.b7166327.png"
+  },
+  {
+    "revision": "d08ca70a826f46806a1709d6bf82368c",
+    "url": "/EMS-Solution/static/media/alphard.d08ca70a.png"
+  },
+  {
+    "revision": "b598496d8c8afe7813c21987c791c3d3",
+    "url": "/EMS-Solution/static/media/avanzamerah.b598496d.png"
+  },
+  {
+    "revision": "ce3caf9e26ca19010f615b61d5e18822",
+    "url": "/EMS-Solution/static/media/fuso1.ce3caf9e.png"
+  },
+  {
+    "revision": "342001ebde80872d85182d3909f8a28f",
+    "url": "/EMS-Solution/static/media/fuso2.342001eb.png"
+  },
+  {
+    "revision": "7ac7c797bf97fff6a92fbf96fbd1a9f0",
+    "url": "/EMS-Solution/static/media/johnDow.7ac7c797.jpg"
+  },
+  {
+    "revision": "ca9719abe86752fc88a56c602f37c42a",
+    "url": "/EMS-Solution/static/media/landingimage.ca9719ab.jpg"
+  },
+  {
+    "revision": "483466fc7575fc44476aff1a8cfe6e61",
+    "url": "/EMS-Solution/static/media/pajero-rf.483466fc.png"
+  },
+  {
+    "revision": "3bfc4b7bdff6ebee85f484e3a277ad98",
+    "url": "/EMS-Solution/static/media/solution.3bfc4b7b.png"
+  },
+  {
+    "revision": "4562c9602a97cc906c1598bda8181f0b",
+    "url": "/EMS-Solution/static/media/xpander.4562c960.png"
+  }
+]);
